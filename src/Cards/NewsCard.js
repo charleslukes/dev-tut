8b6341b3f93de93ae007928de3a0a6e96b0f2266
Charles Chiakwa <charleslukes28@gmail.com>
2019-11-11 17:39:37 +0100

    add news card to project

---
+++ src/Cards/NewsCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+import "./NewsCard.css";
+
+const NewsCard = ({ lang, title, imgLink }) => {
+  return (
+    <>
+      <div className="d-flex font-weight-bold new-hold my-3">
+        <div>
+          <span className="small-text lang-type">#{lang}</span>
+          <p>{title}</p>
+        </div>
+        <div className="img-display">
+          <Image src={imgLink} fluid />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewsCard;
