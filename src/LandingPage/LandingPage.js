d3b3a4517e0900588336e73e168eac9facaa8d63
Charles Chiakwa <charleslukes28@gmail.com>
2019-12-05 10:21:59 +0100

    update card on landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,55 +3,12 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import NewsCard from "../Cards/NewsCard";
 import Button from "../Buttons/Button";
 import Card from "../Cards/Card";
+import { card } from "../utils/card";
+import { newsCard } from "../utils/newsCard";
 import "./LandingPage.css";
 
 const LandingPage = () => {
-  const num = [
-    {
-      imgLink:
-        "https://www.irishtimes.com/polopoly_fs/1.3942790.1565937760!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
-      lang: "JAVASCRIPT",
-      date: "11.11.2019",
-      title: "This an awesome title for Javascript",
-      author: "Micheal Smith",
-      imgLg: "large"
-    },
-    {
-      imgLink:
-        "https://devclass.com/wp-content/uploads/2019/01/scaled-python.jpeg",
-      lang: "REACT",
-      date: "18.11.2019",
-      title: "This an awesome title for React",
-      author: "John Smith"
-    },
-    {
-      imgLink: "https://miro.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-      lang: "PYTHON",
-      date: "01.11.2019",
-      title: "This an awesome title for Python",
-      author: "Micheal John"
-    }
-  ];
-
-  const news = [
-    {
-      imgLink:
-        "https://www.irishtimes.com/polopoly_fs/1.3942790.1565937760!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
-      lang: "JAVASCRIPT",
-      title: "This an awesome title for Javascript"
-    },
-    {
-      imgLink:
-        "https://devclass.com/wp-content/uploads/2019/01/scaled-python.jpeg",
-      lang: "REACT",
-      title: "This an awesome title for React"
-    },
-    {
-      imgLink: "https://miro.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-      lang: "PYTHON",
-      title: "This an awesome title for Python"
-    }
-  ];
+  const cardValue = card.slice(0, 3);
   return (
     <>
       <div className="mt-4">
@@ -67,16 +24,18 @@ const LandingPage = () => {
             </Col>
             <Col md={4}>
               <div className="d-flex flex-wrap justify-content-center">
-                {num.map(({ imgLg, imgLink, lang, date, title, author }) => (
-                  <Card
-                    imgLg={imgLg}
-                    imgLink={imgLink}
-                    lang={lang}
-                    date={date}
-                    title={title}
-                    author={author}
-                  />
-                ))}
+                {cardValue.map(
+                  ({ imgLg, imgLink, lang, date, title, author }) => (
+                    <Card
+                      imgLg={imgLg}
+                      imgLink={imgLink}
+                      lang={lang}
+                      date={date}
+                      title={title}
+                      author={author}
+                    />
+                  )
+                )}
               </div>
             </Col>
             <Col md={3}>
@@ -98,7 +57,7 @@ const LandingPage = () => {
                   <div>Popular</div>
                 </div>
 
-                {news.map(({ imgLink, lang, title }) => (
+                {newsCard.map(({ imgLink, lang, title }) => (
                   <NewsCard imgLink={imgLink} lang={lang} title={title} />
                 ))}
 
