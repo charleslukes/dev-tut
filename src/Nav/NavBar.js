05f4a54ed5947344eb2dea891a2a64fb486b380c
Charles Chiakwa <charleslukes28@gmail.com>
2019-12-05 10:22:37 +0100

    add routing link to nav bar

--- src/Nav/NavBar.js
+++ src/Nav/NavBar.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <>
       <ul className="nav">
-        <li className="main-logo">Dev-Tut</li>
-        <li>Articles</li>
-        <li>News</li>
+        <li className="main-logo">
+          <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+            Dev-Tut
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/articles"
+            style={{ textDecoration: "none", color: "#000" }}
+          >
+            Articles
+          </Link>
+        </li>
+        <li>
+          <Link to="/News" style={{ textDecoration: "none", color: "#000" }}>
+            News
+          </Link>
+        </li>
         <li>Stories</li>
         <li>Meet a dev</li>
         <li className="search-list">
@@ -16,10 +32,10 @@ const NavBar = () => {
             type="text"
             placeholder="search for articles"
           />
-          <i class="fas fa-search"></i>
+          <i className="fas fa-search"></i>
         </li>
         <li>
-          <i class="far fa-user"></i>
+          <i className="far fa-user"></i>
         </li>
       </ul>
     </>
