e899307a8616c315540c36acab8edeb95d1c40b8
Charles Chiakwa <charleslukes28@gmail.com>
2019-12-05 10:22:57 +0100

    mock values

---
+++ src/utils/newsCard.js
@@ -0,0 +1,19 @@
+export const newsCard = [
+  {
+    imgLink:
+      "https://www.irishtimes.com/polopoly_fs/1.3942790.1565937760!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+    lang: "JAVASCRIPT",
+    title: "This an awesome title for Javascript"
+  },
+  {
+    imgLink:
+      "https://devclass.com/wp-content/uploads/2019/01/scaled-python.jpeg",
+    lang: "REACT",
+    title: "This an awesome title for React"
+  },
+  {
+    imgLink: "https://miro.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
+    lang: "PYTHON",
+    title: "This an awesome title for Python"
+  }
+];
