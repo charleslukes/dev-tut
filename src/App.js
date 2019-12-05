6a6c3c8eb7953ec702607df34dac400818a725cb
Charles Chiakwa <charleslukes28@gmail.com>
2019-12-05 10:21:07 +0100

    add routing on app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import NavBar from "./Nav/NavBar";
 import LandingPage from "./LandingPage/LandingPage";
+import Articles from "./Articles/Articles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <LandingPage />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/articles" component={Articles} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
