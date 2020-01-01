89d2aa86a88ed923ce446326bca1dd3c826dea25
charleslukes <charleslukes28@gmail.com>
2020-01-01 20:42:48 +0100

    add user input validation for sign in and sign up

---
+++ src/Sign/validation.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+export const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const nameRegex = /[a-zA-Z]{3,}/;
+
+export const passwordRegex = /(?=.{8,})/;
